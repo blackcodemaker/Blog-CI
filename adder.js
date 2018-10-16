b0c0b6d7d4f0a7c0caf04e15a9f92216e8188bb2
fulminatedaegis <blackcodemaker@users.noreply.github.com>
2018-10-15 19:47:03 -0700

    Initial Commit
    Initial Commit

---
+++ adder.js
@@ -0,0 +1,8 @@
+function adder(a, b) {
+  if (!(typeof a === 'number' && typeof b === 'number')) {
+    throw Error('Oh no!');
+  }
+  return a + b;
+}
+
+module.exports = adder;
---
+++ test/test-fizz-buzzer.js
@@ -0,0 +1,21 @@
+const expect = require('chai').expect;
+const num = require('../fizzBuzzer.js')
+
+describe ('num' , function(){
+  it('should be divisible by 15,5,3 and be a number.', function(){
+
+  const testCases = [['the',`Error: 'num' must be a number`],
+                     [15, 'fizz-buzz'], 
+                     [3, 'fizz'], 
+                     [5, 'buzz'],
+                     [7, 7]];
+  for (let x = 0; x < testCases.length; x++){
+    if(isNaN(testCases[x][0])){
+      expect(function() {num(testCases[x][0])}).to.throw(Error);
+    } else {
+      expect(num(testCases[x][0])).to.equal(testCases[x][1]);
+    }
+  }
+  
+  });
+});
